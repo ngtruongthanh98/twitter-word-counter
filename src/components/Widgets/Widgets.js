3bdeba35e29f9394db579bb2998fec3b1267704e
thanhnguyen98 <ngtruongthanh98@gmail.com>
2021-03-18 16:39:55 +0700

    work with post and count input words

--- src/components/Widgets/Widgets.js
+++ src/components/Widgets/Widgets.js
@@ -1,13 +1,38 @@
-import React, { PureComponent } from 'react';
+import React from "react";
+import "./Widgets.css";
+import {
+    TwitterTimelineEmbed,
+    TwitterShareButton,
+    TwitterTweetEmbed,
+} from "react-twitter-embed";
+import SearchIcon from "@material-ui/icons/Search";
 
-class Widgets extends PureComponent {
-    render() {
-        return (
-            <div className="widgets">
-                <h2>Widgets</h2>
+function Widgets() {
+    return (
+        <div className="widgets">
+            <div className="widgets__input">
+                <SearchIcon className="widgets__searchIcon" />
+                <input placeholder="Search Twitter" type="text" />
             </div>
-        );
-    }
+
+            <div className="widgets__widgetContainer">
+                <h2>What's happening</h2>
+
+                <TwitterTweetEmbed tweetId={"1372480576058318850"} />
+
+                <TwitterTimelineEmbed
+                    sourceType="profile"
+                    screenName="drazennguyen"
+                    options={{ height: 400 }}
+                />
+
+                <TwitterShareButton
+                    url={"https://facebook.com/cleverprogrammer"}
+                    options={{ text: "#reactjs is awesome", via: "cleverqazi" }}
+                />
+            </div>
+        </div>
+    );
 }
 
 export default Widgets;
