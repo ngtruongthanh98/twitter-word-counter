4e9ea6fcb368f630d878b860aced603380dd364b
thanhnguyen98 <ngtruongthanh98@gmail.com>
2021-03-18 08:58:05 +0700

    start with Feed component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import "./App.css";
 import React from "react";
 import Slidebar from "./components/Slidebar/Slidebar"
+import Feed from "./components/Feed/Feed"
+import Widgets from "./components/Widgets/Widgets"
 
 function App() {
     return (
         <div className="app">
             <h1>Twitter</h1>
             <Slidebar/>
+            <Widgets/>
+
         </div>
     );
 }
