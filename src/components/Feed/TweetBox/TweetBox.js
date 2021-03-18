96b4f0132bc9aa6e9857d0755e47dab04dd8ab30
thanhnguyen98 <ngtruongthanh98@gmail.com>
2021-03-18 14:27:56 +0700

    try to add other component among the Tweet Button

--- src/components/Feed/TweetBox/TweetBox.js
+++ src/components/Feed/TweetBox/TweetBox.js
@@ -1,6 +1,6 @@
-import React, { PureComponent } from 'react';
-import { Avatar, Button } from '@material-ui/core';
-import "./TweetBox.css"
+import React, { PureComponent } from "react";
+import { Avatar, Button } from "@material-ui/core";
+import "./TweetBox.css";
 
 class TweetBox extends PureComponent {
     render() {
@@ -8,11 +8,14 @@ class TweetBox extends PureComponent {
             <div className="tweetBox">
                 <form>
                     <div className="tweetBox__input">
-                        <Avatar src="https://scontent.fvca1-1.fna.fbcdn.net/v/t1.0-9/157884103_2880654552182638_2842458262959537966_n.jpg?_nc_cat=102&ccb=1-3&_nc_sid=09cbfe&_nc_ohc=jigiLCHGgBMAX-vXngN&_nc_ht=scontent.fvca1-1.fna&oh=36429c6a8803f035a59cf4a0a7bbea33&oe=60778A7C"/>
-                        <input placeholder="What's happening?" type="text"/>
-                        {/* <input placeholder="Enter image URL" type="text"/> */}
-
+                        <Avatar src="https://scontent.fvca1-1.fna.fbcdn.net/v/t1.0-9/157884103_2880654552182638_2842458262959537966_n.jpg?_nc_cat=102&ccb=1-3&_nc_sid=09cbfe&_nc_ohc=jigiLCHGgBMAX-vXngN&_nc_ht=scontent.fvca1-1.fna&oh=36429c6a8803f035a59cf4a0a7bbea33&oe=60778A7C" />
+                        <input placeholder="What's happening?" type="text" />
                     </div>
+                    <input
+                        className="tweetBox__imageInput"
+                        placeholder="Optional: Enter image URL"
+                        type="text"
+                    />
                 </form>
                 <Button className="tweetBox__tweetButton">Tweet</Button>
             </div>
